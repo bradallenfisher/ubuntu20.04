70dcb5fdb79c44a05dc959fb0092a8520c0c771d
Brad Fisher <bradallenfisher@gmail.com>
2016-12-14 17:27:29 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -78,7 +78,6 @@ service mysqld restart
 service php7.0-fpm restart
 service varnish restart
 
-
 # Install Drush globally.
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
