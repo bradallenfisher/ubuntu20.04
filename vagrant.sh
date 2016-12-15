ef89e57a8e87e4fafad5e47b20d63d29e7a75710
Brad Fisher <bradallenfisher@gmail.com>
2016-12-15 18:00:49 -0500

    Update vagrant.sh

--- vagrant.sh
+++ vagrant.sh
@@ -38,27 +38,27 @@ echo request_slowlog_timeout = 2s >> /etc/php/7.0/fpm/pool.d/www.conf
 echo php_admin_value[error_log] = /var/log/php-fpm/www-error.log >> /etc/php/7.0/fpm/pool.d/www.conf
 
 # BASIC PERFORMANCE SETTINGS
-cat performance/compression.conf > /etc/apache2/conf-available/compression.conf
+cat /vagrant/performance/compression.conf > /etc/apache2/conf-available/compression.conf
 a2enconf compression
-cat performance/content_transformation.conf > /etc/apache2/conf-available/content_transformation.conf
+cat /vagrant/performance/content_transformation.conf > /etc/apache2/conf-available/content_transformation.conf
 a2enconf content_transformation
-cat performance/etags.conf > /etc/apache2/conf-available/etags.conf
+cat /vagrant/performance/etags.conf > /etc/apache2/conf-available/etags.conf
 a2enconf etags
-cat performance/expires_headers.conf > /etc/apache2/conf-available/expires_headers.conf
+cat /vagrant/performance/expires_headers.conf > /etc/apache2/conf-available/expires_headers.conf
 a2enconf expires_headers
-cat performance/file_concatenation.conf > /etc/apache2/conf-available/file_concatenation.conf
+cat /vagrant/performance/file_concatenation.conf > /etc/apache2/conf-available/file_concatenation.conf
 a2enconf file_concatenation
-cat performance/filename-based_cache_busting.conf > /etc/apache2/conf-available/filename-based_cache_busting.conf
+cat /vagrant/performance/filename-based_cache_busting.conf > /etc/apache2/conf-available/filename-based_cache_busting.conf
 a2enconf filename-based_cache_busting
 
 # Security Basics
-cat security/security.conf > /etc/apache2/conf-available/security.conf
+cat /vagrant/security/security.conf > /etc/apache2/conf-available/security.conf
 sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=1/g' /etc/php/7.0/fpm/php.ini
 sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=1/g' /etc/php/7.0/cgi/php.ini
 sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=1/g' /etc/php/7.0/cli/php.ini
 
 #opcache settings
-cat php/opcache.ini > /etc/php/7.0/mods-available/opcache.ini
+cat /vagrant/php/opcache.ini > /etc/php/7.0/mods-available/opcache.ini
 
 service apache2 restart
 service mysqld restart
