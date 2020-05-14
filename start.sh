69b0e2f8415ba7a41c68abe27c26fa3f1f40c489
Brad Fisher <bradallenfisher@gmail.com>
2020-05-14 12:45:45 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -24,25 +24,25 @@ apt-get -y install uuid uuid-runtime curl policycoreutils unzip patch git nano g
 apt-get -y install software-properties-common python-software-properties
 add-apt-repository -y ppa:ondrej/php
 apt-get update
-apt-get -y install php7.2 php7.2-common php7.2-mysql php7.2-ldap php7.2-cgi php7.2-xml php7.2-curl php7.2-gd php7.2-cli php7.2-fpm php7.2-dev php7.2-mbstring
+apt-get -y install php7.3 php7.3-common php7.3-mysql php7.3-ldap php7.3-cgi php7.3-xml php7.3-curl php7.3-gd php7.3-cli php7.3-fpm php7.3-dev php7.3-mbstring
 
 apt-get -y install libapache2-mod-fastcgi
 a2enmod actions fastcgi alias proxy_fcgi setenvif
-a2enconf php7.2-fpm
+a2enconf php7.3-fpm
 
-# PHP 7.2 FPM with apache settings
+# PHP 7.3 FPM with apache settings
 cat php/apache_domain_php-fpm.conf > /etc/apache2/sites-available/000-default.conf
 # fix date timezone errors
-sed -i 's#;date.timezone =#date.timezone = "America/New_York"#g' /etc/php/7.2/fpm/php.ini
+sed -i 's#;date.timezone =#date.timezone = "America/New_York"#g' /etc/php/7.3/fpm/php.ini
 
 # enable apache headers
 a2enmod ssl rewrite headers
 
 # Sanity Logs
 mkdir /var/log/php-fpm/
-echo slowlog = /var/log/php-fpm/www-slow.log >> /etc/php/7.2/fpm/pool.d/www.conf
-echo request_slowlog_timeout = 2s >> /etc/php/7.2/fpm/pool.d/www.conf
-echo php_admin_value[error_log] = /var/log/php-fpm/www-error.log >> /etc/php/7.2/fpm/pool.d/www.conf
+echo slowlog = /var/log/php-fpm/www-slow.log >> /etc/php/7.3/fpm/pool.d/www.conf
+echo request_slowlog_timeout = 2s >> /etc/php/7.3/fpm/pool.d/www.conf
+echo php_admin_value[error_log] = /var/log/php-fpm/www-error.log >> /etc/php/7.3/fpm/pool.d/www.conf
 
 # BASIC PERFORMANCE SETTINGS
 cat performance/compression.conf > /etc/apache2/conf-available/compression.conf
@@ -60,19 +60,19 @@ a2enconf filename-based_cache_busting
 
 # Security Basics
 cat security/security.conf > /etc/apache2/conf-available/security.conf
-sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.2/fpm/php.ini
-sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.2/cgi/php.ini
-sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.2/cli/php.ini
+sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.3/fpm/php.ini
+sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.3/cgi/php.ini
+sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.3/cli/php.ini
 
 #opcache settings
-cat php/opcache.ini > /etc/php/7.2/mods-available/opcache.ini
+cat php/opcache.ini > /etc/php/7.3/mods-available/opcache.ini
 
 #Modules
 a2enmod expires
 
 service apache2 restart
 service mysql restart
-service php7.2-fpm restart
+service php7.3-fpm restart
 
 # Install Drush globally.
 curl -sS https://getcomposer.org/installer | php
