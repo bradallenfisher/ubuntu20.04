d4089d96e30c721529f93a1ed154ce4e25340b25
Brad Fisher <bradallenfisher@gmail.com>
2020-12-30 02:25:30 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -21,10 +21,10 @@ apt -y install php-fpm php-mysql php-ldap php-cgi php-xml php-curl php-gd php-cl
 a2enmod actions fastcgi alias proxy_fcgi setenvif
 a2enconf php7.4-fpm
 
-# PHP 7.3 FPM with apache settings
+# PHP 7.4 FPM with apache settings
 cat /vagrant/php/apache_domain_php-fpm.conf > /etc/apache2/sites-available/000-default.conf
 # fix date timezone errors
-sed -i 's#;date.timezone =#date.timezone = "America/New_York"#g' /etc/php/7.3/fpm/php.ini
+sed -i 's#;date.timezone =#date.timezone = "America/New_York"#g' /etc/php/7.4/fpm/php.ini
 
 # enable apache headers
 a2enmod ssl rewrite headers
